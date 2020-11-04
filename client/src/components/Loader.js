ca7e4908fffe357e8ef36718f71f172f7cae4b63
starprince7 <princeagezinweke@gmail.com>
2020-11-04 13:10:48 +0100

    Upgraded the back Loader component and added a background Opacity and  A Cursor wait style in CSS

--- client/src/components/Loader.js
+++ client/src/components/Loader.js
@@ -10,23 +10,28 @@ function Loader() {
 
     React.useEffect(() => {
         const loader = document.querySelector('.background-loader')
+        const loaderBack = document.querySelector('.loader__dark-background ')
     
         if (loading) {
           loader.style.display = 'block'
+          loaderBack.style.opacity = '1'
+          loaderBack.style.display = 'block'
         }
         else {
           loader.style.display = 'none'
+          loaderBack.style.opacity = '0'
+          loaderBack.style.display = 'none'
         }
       }, [loading])
 
   return (
-    <div>
+    <div className="loader__dark-background">
       {/* Loader Below Here! */}
       <div class="background-loader">
         <div id="loader">
           <img src={KongaLoader} alt="konga loader" />
         </div>
-        <span className="loader__caption"> &copy;Developed And designed By Prince Agezi N.</span>
+        <span className="loader__caption">&copy;Developed And designed By Prince Agezi N.</span>
         <div className="divider"></div>
       </div>
       {/* Loader Above Here! */}
